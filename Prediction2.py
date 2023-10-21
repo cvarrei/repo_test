7a5aa59e75a76a74f3331ccdabc15223ae2b81dc
Clovis Varangot-Reille <113834539+cvarrei@users.noreply.github.com>
2023-10-21 09:29:10 +0200

    Update Prediction2.py
    Ajout commentaires

--- Prediction2.py
+++ Prediction2.py
@@ -1,3 +1,4 @@
+# On importe les packages nécessaires
 import numpy as np
 import pandas as pd
 import sklearn
@@ -11,6 +12,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 
+# On crée une fonction qui enlève les caractères spéciaux
 def norme(df):
     df = df.replace('Î', 'I', regex=True)
     df = df.replace('Ô', 'O', regex=True)
@@ -20,12 +22,16 @@ def norme(df):
     return df
 
 
+# On crée une fonction qui ajoute les variables supplémentaires
 def preproc(tl,sr,nbpp,st,te,nom_departement):
-    
+
+    # On transforme "te" en booléen
     if(te=="y"):
         te=True
     else:
         te=False
+
+    # On renomme les colonnes
     data=pd.DataFrame([[tl,sr,nbpp,st,te,nom_departement]],columns=["Type local","Surface reelle bati","Nombre pieces principales","Surface terrain","exterieur","nom_departement"])
 
         # Concatenation de l'open data
@@ -49,15 +55,16 @@ def preproc(tl,sr,nbpp,st,te,nom_departement):
     df=norme(df)
     data = pd.merge(data, df, on="nom_departement", how="left")
 
+    # On enlève code_departement
     data.drop(columns=["code_departement"], inplace=True)
+    data.drop(columns=["nom_departement"], inplace=True)
 
     return data
 
 
-
+# On crée une fonction qui selon la modalité utilisera le fichier pickle adéquat de préprocessing (si modèle le nécessitant) et utilise le modèle de prédiction correspondant
 def pred(data):
-    pd.set_option('display.max_columns', None)
-    print(data)
+    
     if (data["Type local"][0]=="Maison"):
         data.drop(columns="Type local", inplace=True)
         data["estimated"] = data["Surface reelle bati"] * data["q3_prixm2"]
@@ -90,21 +97,13 @@ def pred(data):
         predi=regression_dep.predict(datat)
 
     elif (data["Type local"][0]=="local"):
-        print("oui")
         data.drop(columns="Type local", inplace=True)
-        print("oui")
         data.drop(columns="nom_departement", inplace=True)
-        print("oui")
         data.drop(columns="Nombre pieces principales", inplace=True)
         data["estimated"] = data["Surface reelle bati"] * data["q1_prixm2"]
-        print("oui2")
         with open("Model/local_model.pkl", 'rb') as file_reg:
             regression_loc = pickle.load(file_reg)
-        print("oui3")
         predi=regression_loc.predict(data)
-
-    print("#####################################")
-    print(predi)
 
 
     return predi
