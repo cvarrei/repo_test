792e1e2736fba414ccb610287061561f8e4efdcf
Clovis Varangot-Reille <113834539+cvarrei@users.noreply.github.com>
2023-10-21 11:09:05 +0200

    Update app_v4.py

--- app_v4.py
+++ app_v4.py
@@ -13,6 +13,23 @@ from qualitative_prep import Qual_Standardize
 import numpy as np
 from sklearn.base import TransformerMixin
 
+class Qual_Standardize(TransformerMixin):
+        # On standardize les valeurs qualitatives en utilisant la racine carré de p_k.
+        def __init__(self):
+            self.p_k = None
+        # Notre fit calcule la valeur p_k nécessaire à la transformation.
+        def fit(self, X, y=None):
+            qual_int = X.astype(int)
+            # On calcule la valeur p_k comme la proportion de True dans la colonne
+            self.p_k = np.sum(qual_int, axis=0) / qual_int.shape[0]
+            return self
+        def transform(self, X, y=None):
+            qual_int = X.astype(int)    
+            # On transforme chaque valeur du tableau disjonctif complet par la racine carré de p_k
+            qual_trans = qual_int / (np.sqrt(self.p_k))
+
+            return qual_trans
+            
 df=pd.read_csv("df_dash_dashboard.csv") # On importe le jeux de données
 
 ############### Nettoyage pour faciliter la représentation graphique ###############
