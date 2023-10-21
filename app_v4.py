67bcd39c373b02cd6fef663b9510d862cab40863
Clovis Varangot-Reille <113834539+cvarrei@users.noreply.github.com>
2023-10-21 10:56:05 +0200

    Update app_v4.py

--- app_v4.py
+++ app_v4.py
@@ -722,24 +722,6 @@ def mettre_a_jour_output(n_clicks,  tl, sr, nbpp, st, te, nom_departement):
     else:
         return "En attente"
 
-# On ajoute un objet qui nous permet de standardiser les variables catégoriques one-hot encodées.
-class Qual_Standardize(TransformerMixin):
-        # On standardize les valeurs qualitatives en utilisant la racine carré de p_k.
-        def __init__(self):
-            self.p_k = None
-        # Notre fit calcule la valeur p_k nécessaire à la transformation.
-        def fit(self, X, y=None):
-            qual_int = X.astype(int)
-            # On calcule la valeur p_k comme la proportion de True dans la colonne
-            self.p_k = np.sum(qual_int, axis=0) / qual_int.shape[0]
-            return self
-        def transform(self, X, y=None):
-            qual_int = X.astype(int)    
-            # On transforme chaque valeur du tableau disjonctif complet par la racine carré de p_k
-            qual_trans = qual_int / (np.sqrt(self.p_k))
-
-            return qual_trans
-            
 if __name__ == '__main__':
     # On lance le serveur.
     app.run_server(debug=True)
