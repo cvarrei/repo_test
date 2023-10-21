88a302a852ee919dbf842d8b2898d9d0155b8344
Clovis Varangot-Reille <113834539+cvarrei@users.noreply.github.com>
2023-10-21 11:08:52 +0200

    Update Prediction2.py

--- Prediction2.py
+++ Prediction2.py
@@ -12,7 +12,23 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
 from qualitative_prep import Qual_Standardize
 
+class Qual_Standardize(TransformerMixin):
+        # On standardize les valeurs qualitatives en utilisant la racine carré de p_k.
+        def __init__(self):
+            self.p_k = None
+        # Notre fit calcule la valeur p_k nécessaire à la transformation.
+        def fit(self, X, y=None):
+            qual_int = X.astype(int)
+            # On calcule la valeur p_k comme la proportion de True dans la colonne
+            self.p_k = np.sum(qual_int, axis=0) / qual_int.shape[0]
+            return self
+        def transform(self, X, y=None):
+            qual_int = X.astype(int)    
+            # On transforme chaque valeur du tableau disjonctif complet par la racine carré de p_k
+            qual_trans = qual_int / (np.sqrt(self.p_k))
 
+            return qual_trans
+            
 # On crée une fonction qui enlève les caractères spéciaux
 def norme(df):
     df = df.replace('Î', 'I', regex=True)
